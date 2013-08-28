1c9928f14b894d80abf497f5e96bd894a82ca950
cconnolly12 <cconnolly12@yahoo.com>
2013-08-27 19:30:47 -0500

    Create getstrongbox.com - nav.php
    Navigation for getstrongbox.com micro site

---
+++ getstrongbox.com - nav.php
@@ -0,0 +1,17 @@
+<nav id="menu">
+     <div class="container">
+          <ul id="nav">
+               <li><a href="#scroll1" title="Why">WHY STRONGBOX</a> </li>
+               <li><a href="#scroll2" title="Open">OPEN STRONGBOX</a> </li>
+               <li><a href="#scroll3" title="Empower">EMPOWER YOUR ARCHIVE</a></li>
+               <li><a href="#scroll4" title="Platform">SB PLATFORM</a> </li>
+               <li><a href="#scroll5" title="Track">TRACK STRONGBOX</a> </li>
+               <li><a href="#scroll6" title="Connect">CONNECT</a></li>
+          </ul>
+          <!--/ end #nav--> 
+     </div>
+     <!--/ end .container--> 
+</nav>
+<!--/ end #menu--> 
+
+<a href="#" class="scrollup">Scroll</a>
