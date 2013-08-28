1ca34c8ca3ab9552b57c961401a7e59477f59e98
cconnolly12 <cconnolly12@yahoo.com>
2013-08-27 19:29:12 -0500

    Create getstrongbox.com - header.php
    header file for getstrongbox.com

---
+++ getstrongbox.com - header.php
@@ -0,0 +1,5 @@
+<div id="scroll0"></div>
+<header>
+<div id="logo"><a href="#scroll1" title="Strongbox Home"><img src="../images/logo.png" alt="www.getstrongbox.com" /></a></div>
+<div id="crds_wrapper"><a alt="www.crossroads.com" href="http://www.crossroads.com" target="_blank">Visit crossroads.com for more info.</a></div> 
+</header>
