0e28b31995294d21d35d4ece588b43c1c67a43cc
cconnolly12 <cconnolly12@yahoo.com>
2013-08-28 08:22:38 -0500

    Create getstrongbox.com - why-strongbox.php
    Why StrongBox for Getstrongbox.com micro site

---
+++ getstrongbox.com - why-strongbox.php
@@ -0,0 +1,48 @@
+<div class="elements">
+     <div class="inner stageBlur innerSpeed5"  style="margin-top:-50px;"></div>
+</div>
+<!-- SLIDE IN -->
+
+<div class="sectionWrap">
+
+     <div class="section section2">
+
+          <div class="wrapper">
+
+               <div class="contentLeft">
+                    <h2>Why StrongBox?</h2>
+                    <p>Discover why these media and entertainment companies have leveraged the reliability and cost-effectiveness of an LTFS-based shared storage solution. The scalability, high-performance and intelligent storage architecture behind StrongBox empowers them to master their archive, from boutique post production house to major broadcast network. </p>
+                       
+                         <!-- <a href="/assets/mlb-case-study.mp4" id="bottomCase"><div class="bottomText">View our case studies</div></a></div> -->  
+               </div>
+               
+               
+               <div class="contentRight">
+                    <div id="computer">
+                         <link rel="stylesheet" href="/css/classic.css"/>
+                         <!-- start flowplayer-->
+                         <div id="classic" class="flowplayer is-splash is-closeable" data-ratio="0.5625">
+                              <video preload='none'>
+                                   
+                                   <source type="video/mp4"   src="/assets/prod-for-case-study.mp4"/>
+                                   <source type="video/mp4" src="/assets/cyark-case-study.mp4"/>
+                                   
+                              </video>
+                        
+         
+                              
+                              <div class="fp-playlist">
+                              <div class="bottomCase"><a href="/assets/prod-for-case-study.mp4" id="arrow1"><div class="bottomText">View our case studies</div></a></div>
+                               <a href="/assets/prod-for-case-study.mp4" id="thumb2"></a> <a href="/assets/cyark-case-study.mp4" id="thumb3"></a> </div>
+                              </div> 
+  
+                    </div>
+                    
+                        <div style="float:right; padding:25px 230px 0 10px; z-index:999;"><a href="/pdf/CRDS-StrongBox-Success-Story-MLB-Network.pdf" target="_blank"><img src="/assets/thumb5.png" /></a></div>                      
+                     
+                    <div id="mobile">
+                    </div>
+               </div>
+          </div>
+     </div><div class="tower2"></div> 
+</div>
