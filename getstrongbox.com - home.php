7097a19083975993c53f23781186f02f655c315f
cconnolly12 <cconnolly12@yahoo.com>
2013-08-27 19:30:04 -0500

    Create getstrongbox.com - home.php
    Home page for getstrongbox.com micro site

---
+++ getstrongbox.com - home.php
@@ -0,0 +1,15 @@
+<div class="sectionWrap">
+     <div class="section section1">
+ 
+          <div class="wrapper">
+               <div class="videopane">
+               
+                    <iframe src="http://player.vimeo.com/video/57610361" width="100%" height="435" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+               </div>  
+               
+               <h1>StrongBox LTFS NAS Archive and Shared Storage Solution</h1>
+          </div>
+     </div>
+</div>
+
+<div class="tower1"></div> 
